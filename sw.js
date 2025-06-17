1af3820435999eae397dbe7ce15196c4943b90c3
RO486666 <ro486@yahoo.com>
2025-06-17 23:42:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ const urlsToCache = [
   "/index.html",
   "/styles.css",
   "/script.js",
-  "/icon-192.png",
-  "/icon-512.png"
+  "/app/icon-192.png",
+"/app/icon-512.png"
 ];
 
 self.addEventListener('install', event => {
