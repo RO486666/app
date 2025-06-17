26138cbaac0c8a76e33890e4c889e185d3c4f00f
RO486666 <ro486@yahoo.com>
2025-06-17 23:01:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,20 +2,16 @@ const CACHE_NAME = "trade-app-cache-v1";
 const urlsToCache = [
   "/",
   "/index.html",
-  // Hier weitere Assets eintragen, z.B. CSS, JS, Bilder
+  "/styles.css",
+  "/script.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('my-cache-name').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/script.js',
-        '/icons/icon-192.png',  // <- häufige Fehlerquelle
-        // weitere Ressourcen...
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
