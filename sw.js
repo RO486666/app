ba9a1d32fc7e749d4fb417de721e591b3e7cb231
RO486666 <ro486@yahoo.com>
2026-07-25 13:49:53 +0200

    Auto-Deploy Update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "alphaos-v20260725-132110";
+const CACHE_NAME = "alphaos-v20260725-134953";
 
 // ✅ KORRIGIERT: Nur die Basis-Dateien cachen. 
 // Keine CSS/JS Dateien hier angeben, wenn man sich beim Pfad unsicher ist!
